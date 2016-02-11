748d4766c300cc00f9c432aa1694303189cf961b
Azimath <npnickster@gmail.com>
2016-02-11 11:29:15 -0500

    Made button operate on release

--- ucf_sub_embedded_ros/ucf_sub/src/sub_sensors/src/StartButton.py
+++ ucf_sub_embedded_ros/ucf_sub/src/sub_sensors/src/StartButton.py
@@ -10,8 +10,10 @@ gpio.setup(CHANNEL, gpio.IN)
 if __name__ == '__main__':
     rospy.init_node("StartButton")
     start_pub = rospy.Publisher("/start", Bool, queue_size=10)
-    r = rospy.Rate(4)
+    r = rospy.Rate(30)
     while not rospy.is_shutdown():
-        if not gpio.input(CHANNEL):
-            start_pub.publish(Bool(True))
-        r.sleep()
+        while gpio.input(CHANNEL):
+            r.sleep()
+        while not gpio.input(CHANNEL):
+            r.sleep()
+        start_pub.publish(Bool(True))
