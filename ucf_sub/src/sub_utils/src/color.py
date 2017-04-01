dcb7e5f948bd84c497fa6bbd5c84a2d93782b068
rfatt <rfatt13@gmail.com>
2017-04-01 16:22:47 -0400

    Added comments and set up dumping to different directories based on color

--- ucf_sub/src/sub_utils/src/color.py
+++ ucf_sub/src/sub_utils/src/color.py
@@ -6,16 +6,27 @@ from imutils import paths
 import imutils 
 import os
 from sklearn.externals import joblib
-import time
 
+
+#Argument Parsing
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--positive", required=True, help="path to positive images directory")
 ap.add_argument("-n", "--negative", required=True, help="path to negative images directory")
-ap.add_argument("--pf", required = True, help="path to positive features directory")
 ap.add_argument("--nf", required = True, help="path to negative feature directory")
+ap.add_argument("-r", required = True, help="path to red features directory")
+ap.add_argument("-g", required = True, help="path to green features directory")
+ap.add_argument("-y", required = True, help="path to yellow features directory")
 args = vars(ap.parse_args())
 
-t0 = time.time()
+'''This Loop goes through every image in the specified directory and does the following:
+	1. Read in the image
+	2. Resize it to a constant size
+	3. Split color channels
+	4. Calculate color histogram for each channel and concatenate
+	5. Flatten feature vector
+	6. Determine which color buoy this is
+	7. Dump to appropriate file
+''' 
 for imagePath in paths.list_images(args["positive"]):
 	image = cv2.imread(imagePath)
 	image = imutils.resize(image, width=64)
@@ -34,9 +45,20 @@ for imagePath in paths.list_images(args["positive"]):
 
 	features = np.asarray(features)
 	features = features.flatten()
-	fd_name = os.path.split(imagePath)[1].split(".")[0] + ".feat"
-	fd_path = os.path.join(args["pf"], fd_name)
-	joblib.dump(features, fd_path)
+	if("R" in imagePath):
+		fd_name = os.path.split(imagePath)[1].split(".")[0] + ".feat"
+		fd_path = os.path.join(args["r"], fd_name)
+		joblib.dump(features, fd_path)
+
+	if("Y" in imagePath):
+		fd_name = os.path.split(imagePath)[1].split(".")[0] + ".feat"
+		fd_path = os.path.join(args["y"], fd_name)
+		joblib.dump(features, fd_path)
+
+	if("G" in imagePath):
+		fd_name = os.path.split(imagePath)[1].split(".")[0] + ".feat"
+		fd_path = os.path.join(args["g"], fd_name)
+		joblib.dump(features, fd_path)		
 
 for imagePath in paths.list_images(args["negative"]):
 	image = cv2.imread(imagePath)
@@ -61,9 +83,7 @@ for imagePath in paths.list_images(args["negative"]):
 	joblib.dump(features, fd_path)
 
 
-	'''plt.plot(hist, color = color)
-	plt.xlim([0, 256])
-	plt.show()
-t1 = time.time()
-print(t1-t0
+'''plt.plot(hist, color = color)
+plt.xlim([0, 256])
+plt.show()
 print "flattened feature vector size: %d" % (np.array(features).flatten().shape)'''
