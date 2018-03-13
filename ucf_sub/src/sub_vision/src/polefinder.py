94081ee27f039f406c33cfb27a46b519e37e2de2
Otaku-AI <rfatt13@gmail.com>
2018-03-13 15:48:13 -0400

    init pole finder

---
+++ ucf_sub/src/sub_vision/src/polefinder.py
@@ -0,0 +1,30 @@
+import cv2
+import imutils
+import numpy as np
+import vision_utils
+
+class PoleFinder:
+	def __init__(self):
+		pass
+
+def process(self, imageLeftRect, imageRightRect, imageDisparityRect, cameraModel, stereoCameraModel):
+	imageHSV = cv2.cvtColor(imageLeftRect, cv2.COLOR_BGR2HSV)
+	resized = imutils.resize(imageHSV, width=300)
+	contours, _ = ThreshAndContour(resized, Thresholds(upper=(237,95,78.4),lower=(204,51,100)))
+
+	if len(contours) == 0:
+		return None
+
+	pole = []
+
+	for contour in contours:
+
+		M = cv2.moments(c)
+		if cv2.contourArea(c) > 1000:
+			cX = int(M["m10"] / M["m00"])
+			cY = int(M["m01"] / M["m00"])
+			pole.append(c)
+
+poleCenter = (cX,cY)
+
+return caneraModel.projectPixelTo3dRay(poleCenter)
