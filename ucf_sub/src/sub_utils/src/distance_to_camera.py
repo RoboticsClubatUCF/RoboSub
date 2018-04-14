df7a8a5c3efc433fdb01344d2b92a6d0901d9caf
Otaku-AI <rfatt13@gmail.com>
2018-04-14 11:03:55 -0400

    Set constants

--- ucf_sub/src/sub_utils/src/distance_to_camera.py
+++ ucf_sub/src/sub_utils/src/distance_to_camera.py
@@ -1,22 +1,21 @@
 import numpy as np
 import cv2
 
-
-def distance_to_camera(knownWidth, focalLength, perWidth):
-    return (knownWidth * focalLength) / perWidth
+def distance_to_camera(KNOWN_HEIGHT, focalLength, perWidth):
+    return (KNOWN_HEIGHT * focalLength) / perWidth
 
 
 def findFocalLength(marker, focalLength)
-    return (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+    return (marker[1][0] * KNOWN_DISTANCE) / KNOWN_HEIGHT
 
 
 def findDistance(marker, focalLength):
-    KNOWN_DISTANCE = 0
-    KNOWN_WIDTH = 0
+    KNOWN_DISTANCE = 24
+    KNOWN_HEIGHT = 57
 
     if focalLength == None:
         focalLength = findFocalLength(marker, focalLength)
 
-    inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+    inches = distance_to_camera(KNOWN_HEIGHT, focalLength, marker[1][0])
 
     return inches, focalLength
