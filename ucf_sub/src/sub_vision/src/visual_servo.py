928b2f2a71033dfc2062688c550114bd9abfcbc0
Otaku-AI <rfatt13@gmail.com>
2018-05-12 03:07:16 +0000

    added actionlib server

--- ucf_sub/src/sub_vision/src/visual_servo.py
+++ ucf_sub/src/sub_vision/src/visual_servo.py
@@ -2,13 +2,12 @@ import rospy
 import image_geometry
 from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
-from geometry_msgs.msg import Wrench
 import particle
-
+from sub_vision.msg import VisualServoAction, VisualServoGoal, VisualServoFeedback, VisualServoResult
 
 class visual_servo:
 
-	def __init__(self):
+	def __init__(self,goal):
 		self.particleNum = 1000
 		self.imageWidth = 640
 		self.imageHeight = 480
@@ -16,13 +15,16 @@ class visual_servo:
 		self.yThreshold = 0
 		self.lam = 1
 		self.message = Wrench()
+		self.goal = goal
 		self.particles == particle.initParticles(self.particleNum, self.imageHeight, self.imageWidth)
 		self.thrusterPublisher = rospy.Publisher('desiredThrustWrench', Wrench, queue_size=1)
 		self.box_sub = rospy.Subscriber("/object_bounding_box", numpy_msg(Floats), self.servoing)
 
+
+
 	def interaction_matrix(coordinates):
 		u = self.lam * (coordinates[0]/coordinates[2])
-		v = self.lamb * (coordinates[1]/coordinates[2]) 
+		v = self.lamb * (coordinates[1]/coordinates[2])
 		Z = coordinates[2]
 		return np.array([[-self.lam/Z, 0, u/Z, (u*v)/self.lam,-(self.lam + (math.square(u)/self.lam)), v], [0,-self.lam/Z,v/Z, self.lam + math.square(v)/self.lam, -((u*v)/self.lam), -u]])
 
@@ -37,7 +39,6 @@ class visual_servo:
 			if math.fabs(msg[1]-cY) > self.yThreshold:
 				self.particles = particle.update(self.particles, (cX,cY))
 				self.particles = particle.resample_particles(self.particles, self.particleNum)
-				
 				#Find new desired camera position
 				newPosition = findBestParticle(self.particles)
 				coordinates = (self.particles[newPosition][0],self.particles[newPosition][1])
@@ -45,8 +46,7 @@ class visual_servo:
 				newPosition = image_geometry.projectPixelTo3d(rectCoord)
 				newCommand = np.matmul(np.linalg.pinv(interaction_matrix(newPosition,self.lamb)),rectCoord)
 
-				#Create Wrench 
-				#Todo: Figure out how to convert desired camera position to desired force and torque amounts
+				#Create Wrench
 				message.force.x = newCommand[1]
 				message.force.y = newCommand[0]
 				message.force.z = newCommand[2]
@@ -55,7 +55,12 @@ class visual_servo:
 				message.torque.z = newCommand[2]
 				thrusterPublisher.publish(message)
 
-def main(args):
-	vs = visual_servo()
+def start_servo(self, goal):
+	vs = visual_servo(goal)
 	rospy.init_node('visual_servo', anonymous=False)
 	rospy.spin()
+
+if __name__== "__main__":
+	self.server = actionlib.SimpleActionServer('visual_servo', VisualServoAction, start_servo, False)
+        self.server.start()
+
