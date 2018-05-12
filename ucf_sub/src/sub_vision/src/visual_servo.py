fac1c670312efdadeca0602b9b7d5adb0afc4fae
Otaku-AI <rfatt13@gmail.com>
2018-05-12 06:34:27 +0000

    Started adding pole servoing

--- ucf_sub/src/sub_vision/src/visual_servo.py
+++ ucf_sub/src/sub_vision/src/visual_servo.py
@@ -4,6 +4,7 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 import particle
 from sub_vision.msg import VisualServoAction, VisualServoGoal, VisualServoFeedback, VisualServoResult
+from scipy.optimize import minimize
 
 class visual_servo:
 
@@ -16,19 +17,31 @@ class visual_servo:
 		self.lam = 1
 		self.message = Wrench()
 		self.goal = goal
-		self.particles == particle.initParticles(self.particleNum, self.imageHeight, self.imageWidth)
-		self.thrusterPublisher = rospy.Publisher('desiredThrustWrench', Wrench, queue_size=1)
 		self.box_sub = rospy.Subscriber("/object_bounding_box", numpy_msg(Floats), self.servoing)
+		if goal == VisualServoGoal.gate:
+			self.particles == particle.initParticles(self.particleNum, self.imageHeight, self.imageWidth)
+			self.thrusterPublisher = rospy.Publisher('desiredThrustWrench', Wrench, queue_size=1)
+		elif goal == VisualServoGoal.pole
+			self.fx = image_geometry.fx
+			self.fy = image_geometry.fy
+			self.knownWidth = 0.0
 
-
-
-	def interaction_matrix(coordinates):
+	def interaction_matrix(self,coordinates):
 		u = self.lam * (coordinates[0]/coordinates[2])
 		v = self.lamb * (coordinates[1]/coordinates[2])
 		Z = coordinates[2]
 		return np.array([[-self.lam/Z, 0, u/Z, (u*v)/self.lam,-(self.lam + (math.square(u)/self.lam)), v], [0,-self.lam/Z,v/Z, self.lam + math.square(v)/self.lam, -((u*v)/self.lam), -u]])
 
+	def distance_to_object(self,perWidth):
+		return (self.knownWidth * self.fx) / perWidth)
+
 	def servoing(self, msg):
+		if self.goal == VisualServoGoal.pole:
+			#Find distance to pole
+			distance_to_object(self.msg[1][0])
+				#Generate Virtual Images constrained by other 5 DOF
+					#Find distance for each image
+						#Pick x that maintains distance
 
 		#Find the camera position in the frame
 		cX = image_geometry.cX
@@ -42,17 +55,16 @@ class visual_servo:
 				#Find new desired camera position
 				newPosition = findBestParticle(self.particles)
 				coordinates = (self.particles[newPosition][0],self.particles[newPosition][1])
-				rectCoord = image_geometry.rectifyPoint(coordinates)
-				newPosition = image_geometry.projectPixelTo3d(rectCoord)
-				newCommand = np.matmul(np.linalg.pinv(interaction_matrix(newPosition,self.lamb)),rectCoord)
+				newPosition = image_geometry.projectPixelTo3d(coordinates)
+				newCommand = np.matmul(np.linalg.pinv(interaction_matrix(newPosition,self.lamb)),coordinates)
 
 				#Create Wrench
 				message.force.x = newCommand[1]
 				message.force.y = newCommand[0]
 				message.force.z = newCommand[2]
-				message.torque.x = newCommand[0]
-				message.torque.y = newCommand[1]
-				message.torque.z = newCommand[2]
+				message.torque.x = newCommand[3]
+				message.torque.y = newCommand[4]
+				message.torque.z = newCommand[5]
 				thrusterPublisher.publish(message)
 
 def start_servo(self, goal):
