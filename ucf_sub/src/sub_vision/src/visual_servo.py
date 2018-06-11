f4d8e9b84cbd75cd172596bb48baf9f9b83af12f
Azimath <npnickster@gmail.com>
2018-06-11 06:57:36 -0400

    Added feature calculation

--- ucf_sub/src/sub_vision/src/visual_servo.py
+++ ucf_sub/src/sub_vision/src/visual_servo.py
@@ -18,10 +18,6 @@ class visual_servo:
 		self.server = actionlib.SimpleActionServer('visual_servo', VisualServoAction,self.execute,False)
 		self.server.start()
 
-		self.particleNum = 1000
-		self.imageWidth = 640
-		self.imageHeight = 480
-
 		self.xThreshold = 0
 		self.yThreshold = 0
 
@@ -39,25 +35,25 @@ class visual_servo:
 		self.result=False
 		self.camera_info_pub = rospy.Subscriber("/stereo/right/camera_info", CameraInfo, self.initInfo)
 		self.vision_feedback = rospy.Subscriber('/track_object/feedback', TrackObjectFeedback, self.servoing)
-                self.thruster_pub = rospy.Publisher('/autonomyWrench', Wrench, queue_size=1)
-                self.thruster_sub = rospy.Subscriber("/autonomyWrench", Wrench, self.republishWrench)
-                self.desired_wrench = rospy.Publisher("/desiredThrustWrench", Wrench, queue_size=1)
+        self.thruster_pub = rospy.Publisher('/autonomyWrench', Wrench, queue_size=1)
+        self.thruster_sub = rospy.Subscriber("/autonomyWrench", Wrench, self.republishWrench)
+        self.desired_wrench = rospy.Publisher("/desiredThrustWrench", Wrench, queue_size=1)
 
 		self.startYaw = None
-                self.FirstIMUCall = True
-                self.imuSubscriber = rospy.Subscriber("/imu/data", Imu, self.initIMU)
-                self.depthSubscriber = rospy.Subscriber("/Depth", Float32, self.initDepth)
-                self.knownWidth = 0.0
-                self.maintainedDistance = 12
-                self.speed = 1
+        self.FirstIMUCall = True
+        self.imuSubscriber = rospy.Subscriber("/imu/data", Imu, self.initIMU)
+        self.depthSubscriber = rospy.Subscriber("/Depth", Float32, self.initDepth)
+        self.knownWidth = 0.0
+        self.maintainedDistance = 12
+        self.speed = 1
 
 
 
 	def initInfo(self, msg):
 		self.camera_info = ig.PinholeCameraModel()
 		self.camera_info.fromCameraInfo(msg)
-                self.fx = self.camera_info.fx()
-                self.fy = self.camera_info.fy()
+        self.fx = self.camera_info.fx()
+        self.fy = self.camera_info.fy()
 
 
 	def interaction_matrix(self, dof, u, v, Z):
@@ -68,10 +64,6 @@ class visual_servo:
 		return (self.knownWidth * self.fx) / perWidth
 
 	def initIMU(self, msg):
-		if self.FirstIMUCall:
-			self.FirstIMUCall = False
-			self.startYaw = msg.orientation.z
-
 		self.orientationX = msg.orientation.x
 		self.orientationY = msg.orientation.y
 		self.orientationZ = msg.orientation.z
@@ -86,47 +78,40 @@ class visual_servo:
 		self.running=True
 
  		if msg.servotask == VisualServoGoal.gate:
-                	self.goal = TrackObjectGoal()
-                	self.goal.objectType = self.goal.startGate
+ 		    self.goal = TrackObjectGoal()
+            self.goal.objectType = self.goal.startGate
 			self.goal.servoing = True
 			self.vision_client.send_goal(self.goal)
-			self.particles == particle.initParticles(self.particleNum, self.imageHeight, self.imageWidth)
 
-                elif msg.servotask == VisualServoGoal.drift:
-                     	self.goal = TrackObjectGoal()
+
+        elif msg.servotask == VisualServoGoal.drift:
+          	self.startYaw = self.orientationZ
+          	self.goal = TrackObjectGoal()
 			self.goal.servoing = True
-        		self.goal.objectType = self.goal.pole
-        		self.vision_client.send_goal(self.goal)
+            self.goal.objectType = self.goal.pole
+        	self.vision_client.send_goal(self.goal)
 
 		elif msg.servotask == VisualServoGoal.align:
-	               	self.goal = TrackObjectGoal()
+	    	self.goal = TrackObjectGoal()
 			self.goal.servoing = True
-                       	self.goal.objectType = self.goal.pole
-                       	self.vision_client.send_goal(self.goal)
+            self.goal.objectType = self.goal.pole
+            self.vision_client.send_goal(self.goal)
 
 		while self.running:
-	                if self.server.is_preempt_requested() or self.server.is_new_goal_available():
-        	               self.running = False
-                	       continue
+	        if self.server.is_preempt_requested() or self.server.is_new_goal_available():
+        	    self.running = False
+                continue
 
 
 		self.response.aligned = False
 		self.server.set_succeeded(self.response)
 
-	def transition(self,gh, result):
-		rospy.loginfo(result)
-		self.response.aligned=True
-		self.server.set_succeeded(self.response)
-
 	def servoing(self,msg):
-		rospy.loginfo(type(msg.feedback))
-		#rospy.loginfo(feedback)
-		#rospy.loginfo(self.vision_client.get_result())
 		if self.goal == VisualServoGoal.drift:
 			if self.orientationZ - self.startYaw < 270:
 				#Find distance to pole
 				interaction = None
-				coordinates = self.camera_info.projectPixelTo3dRay(msg.center)
+				coordinates = self.camera_info.projectPixelTo3dRay(msg.feedback.center)
 				u = self.lam * (coordinates[0]/coordinates[2])
 				v = self.lamb * (coordinates[1]/coordinates[2])
 				Z = coordinates[2]
@@ -146,7 +131,13 @@ class visual_servo:
 				self.response.aligned = True
 				self.server.set_succeeded(self.response)
 
-		else:
+		elif self.goal == VisualServoGoal.gate or self.goal == VisualServoGoal.align:
+			features = []
+			features[0] = (msg.feedback.center[0]-(msg.feedback.width/2),msg.feedback.center[1]-(msg.feedback.height/2))
+			features[1] = (features[0][0]+msg.feedback.width, features[0][1])
+			features[2] = (features[0][0], features[0][1]+msg.feedback.height)
+
+
 			#Find the camera position in the frame
 			cX = self.camera_info.cx()
 			cY = self.camera_info.cy()
@@ -154,24 +145,20 @@ class visual_servo:
 			#If center of bounding box is not aligned with camera
 			if math.fabs(msg.feedback.center[0]-cX) > self.xThreshold:
 				if math.fabs(msg.feedback.center[1]-cY) > self.yThreshold:
-					coordinates = [self.camera_info.projectPixelTo3dRay(msg.feedback.center) for i in range(3)]
+					coordinates = [self.camera_info.projectPixelTo3dRay(features[i]) for i in range(3)]
+					rospy.loginfo(coordinates)
 					u = [self.lam * (coordinates[i][0]/coordinates[i][2]) for i in range(3)]
 					v = [self.lam * (coordinates[i][1]/coordinates[i][2]) for i in range(3)]
 					Z = [coordinates[i][2] for i in range(3)]
 					dof = [0,1,2,3,4,5,6]
-					int_matrix = self.interaction_matrix(dof, u[i], v[i], Z[i])
+					int_matrix = self.interaction_matrix(dof, u[0], v[0], Z[0])
 
 					for i in range(2):
 						int_matrix = np.stack(int_matrix, self.interaction_matrix(dof, u[i+1], v[i+1], Z[i+1]))
 
 
-					self.particles = particle.update(self.particles, (cX,cY))
-					self.particles = particle.resample_particles(self.particles, self.particleNum)
-					#Find new desired camera position
-					newPosition = findBestParticle(self.particles)
-					coordinates = (self.particles[newPosition][0],self.particles[newPosition][1])
-					newPosition = self.camera_info.projectPixelTo3d(coordinates)
-					newCommand = np.matmul(np.linalg.pinv(int_matrix),newPosition)
+					rospy.loginfo(int_matrix)
+					newCommand = np.matmul(np.linalg.pinv(int_matrix), (cX, cY))
 
 					#Create Wrench
 					self.message.force.x = newCommand[1]
