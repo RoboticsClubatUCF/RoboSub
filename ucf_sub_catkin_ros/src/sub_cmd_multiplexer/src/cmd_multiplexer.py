000531f4b0135a351f58b862f50eeddc95f384aa
Azimath <npnickster@gmail.com>
2018-06-29 21:51:47 -0400

    Fixes, made mostly complete

--- ucf_sub_catkin_ros/src/sub_cmd_multiplexer/src/cmd_multiplexer.py
+++ ucf_sub_catkin_ros/src/sub_cmd_multiplexer/src/cmd_multiplexer.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python3
 import rospy
+
 from geometry_msgs.msg import Wrench
 from geometry_msgs.msg import Vector3
+
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float32
+
 from sub_trajectory.msg import StabilityMode
 
+from enum import Enum
+class modes(Enum):
+	off = 1
+	velocity = 2
+	position = 3
 
 class ThrusterControl:
 
@@ -16,90 +24,88 @@ class ThrusterControl:
 		self.limit_pub = rospy.Publisher("/thruster_limit", Float32, queue_size=1000)
 		self.autonomyEnabled = 1
 		self.autonomy_sub = rospy.Subscriber("/autonomyWrench", Wrench, self.republishWrench)
-		self.autonomy_pub = rospy.Publisher("/autonomyWrench", Wrench, queue_size=1000)
 
-		self.stability_pub = rospy.Subscriber("/stabilityWrench", Wrench, self.stabilityWrench)
+		self.stability_sub = rospy.Subscriber("/stabilityWrench", Wrench, self.stabilityWrench)
 
 		self.twistMsg = Wrench()
 		self.stabilityMsg = Wrench()
-		self.depthMode = depthModes.off
-		self.angleMode = angleModes.off
-		self.yawEnabled == False
+		self.depthMode = modes.off
+		self.angleMode = modes.off
+		self.yawEnabled = False
 		self.stabilityMode = StabilityMode()
 		self.depth_mode_pub = rospy.Publisher("/thrusters/depthMode", StabilityMode, queue_size=1)
 		self.angle_mode_pub = rospy.Publisher("/thrusters/angleMode", StabilityMode, queue_size=1)
 
 	def translateCb(self, msg):
-		if(len(msg.axes) < 3):
-			rospy.loginfo("JOYSTICK ERROR")
+		if(len(msg.axes) < 5):
+			rospy.loginfo("JOYSTICK ERROR: Not enough axes")
 			return
 
-		self.twistMsg.force.x = msg.axes[1] * 20
-		self.twistMsg.force.y = msg.axes[0] * 20
-		self.twistMsg.force.z = msg.axes[2] * 20
+		self.twistMsg.force.x = msg.axes[1] * 7
+		self.twistMsg.force.y = msg.axes[0] * 7
+		self.twistMsg.force.z = msg.axes[2] * 7
 		
 		thruster_limit = (msg.axes[3]+1)/4
+		self.limit_pub.publish(thruster_limit)
 
-		if msg.axes[4] == 1 and msg.axes[5] == 0:
+		if msg.axes[4] > 0.5 and abs(msg.axes[5]) < 0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.velocity
 			self.depth_mode_pub.publish(self.stabilityMode)
 
-		elif msg.axes[4] == 0 and msg.axes[5] == 1:
+		elif abs(msg.axes[4]) < 0.5 and msg.axes[5] > 0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.position
 			self.depth_mode_pub.publish(self.stabilityMode)
 
-		elif msg.axes[4] == 0 and msg.axes[5] == -1:
+		elif abs(msg.axes[4]) < 0.5 and msg.axes[5] < -0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.off
 			self.depth_mode_pub.publish(self.stabilityMode)
 
-
-		self.limit_pub.publish(thruster_limit)
 		rospy.loginfo("TRANSLATION UPDATED")
 
 
 	def rotateCb(self, msg):
-		if(len(msg.axes) < 3):
+		if(len(msg.axes) < 5):
 			rospy.loginfo("JOYSTICK ERROR: Not enough axes")
 			return
-		self.twistMsg.torque.x = msg.axes[0] * 20
-		self.twistMsg.torque.y = msg.axes[1] * 20
-		self.twistMsg.torque.z = msg.axes[2] * 20
+		self.twistMsg.torque.x = msg.axes[0] * -7
+		self.twistMsg.torque.y = msg.axes[1] * -7
+		self.twistMsg.torque.z = msg.axes[2] * -7
 		self.autonomyEnabled = msg.axes[3]
 
-		if msg.axes[4] == 1 and msg.axes[5] == 0:
+		if msg.axes[4] > 0.5 and abs(msg.axes[5]) < 0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.velocity
 			self.stabilityMode.yawEnabled = self.yawEnabled
 			self.angle_mode_pub.publish(self.stabilityMode)
 
-		elif msg.axes[4] == -1 and msg.axes[5] == 0:
-			self.stabilityMode.yawEnabled = not self.yawEnabled
-			self.depth_mode_pub.publish(self.stabilityMode)
+		elif msg.axes[4] < -0.5 and abs(msg.axes[5]) < 0.5:
+			self.yawEnabled = not self.yawEnabled
+			self.stabilityMode.yawEnabled = self.yawEnabled
+			self.angle_mode_pub.publish(self.stabilityMode)
 
-		elif msg.axes[4] == 0 and msg.axes[5] == 1:
+		elif abs(msg.axes[4]) < 0.5 and msg.axes[5] > 0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.position
 			self.stabilityMode.yawEnabled = self.yawEnabled
-			self.depth_mode_pub.publish(self.stabilityMode.mode.position)
+			self.angle_mode_pub.publish(self.stabilityMode)
 
-		elif msg.axes[4] == 0 and msg.axes[5] == -1:
+		elif abs(msg.axes[4]) < 0.5 and msg.axes[5] < -0.5:
 			self.stabilityMode.mode = self.stabilityMode.mode.off
 			self.stabilityMode.yawEnabled = self.yawEnabled
-			self.depth_mode_pub.publish(self.stabilityMode.mode.off)
+			self.angle_mode_pub.publish(self.stabilityMode)
 
 		rospy.loginfo("ROTATION UPDATED")
 
-
 	def republishWrench(self, msg):
 		if self.autonomyEnabled > 0:
-			self.msg.force.x = self.msg.force.x + self.stabilityMsg.force.x
-			self.msg.force.y = self.msg.force.y + self.stabilityMsg.force.y
-			self.msg.force.z = self.msg.force.z + self.stabilityMsg.force.z
-			self.msg.torque.x = self.msg.torque.x + self.stabilityMsg.torque.x
-			self.msg.torque.y = self.msg.torque.x + self.stabilityMsg.torque.y
-			self.msg.torque.z = self.msg.torque.x + self.stabilityMsg.torque.z
+			msg.force.x = msg.force.x + self.stabilityMsg.force.x
+			msg.force.y = msg.force.y + self.stabilityMsg.force.y
+			msg.force.z = msg.force.z + self.stabilityMsg.force.z
+			msg.torque.x = msg.torque.x + self.stabilityMsg.torque.x
+			msg.torque.y = msg.torque.y + self.stabilityMsg.torque.y
+			msg.torque.z = msg.torque.z + self.stabilityMsg.torque.z
 			self.twist_pub.publish(msg)
 
 	def stabilityWrench(self, msg):
-		stabilityMsg = msg
+		self.stabilityMsg = msg
 
 if __name__== "__main__":
 	tc = ThrusterControl()
--- ucf_sub_catkin_ros/src/sub_trajectory/src/active_stability.py
+++ ucf_sub_catkin_ros/src/sub_trajectory/src/active_stability.py
@@ -3,75 +3,124 @@ from nav_msgs.msg import Odometry
 from sub_trajectory.msg import StabilityMode
 import tf
 import rospy
+import numpy as np
 
-class depthModes(Enum):
+class modes(Enum):
 	off = 1
 	velocity = 2
 	position = 3
 
-class angleModes(Enum):
-	off = 1
-	velocity = 2
-	position = 3
-	yawEnabled = 4
+def rosToArray(msg): #Convert a ros message with 1-4 dimensions into a numpy array
+    return np.array([getattr(msg, key) for key in ("x", "y", "z", "w") if hasattr(msg, key)])
 
+def rotateVector(quat, vec):
+	quat2 = list(vec).append(0.0)
+	return tf.transformations.quaternion_multiply(
+		tf.transformations.quaternion_multiply(quat, quat2),
+		tf.transformations.quaternion_conjugate(quat)
+	)[:3]
 
-class active_stabilization(self):
+class ActiveStabilizer():
 	def __init__(self):
-		self.curDepthMode = depthModes.off
-		self.curAngleMode = angleModes.off
+		self.curDepthMode = modes.off
+		self.curAngleMode = modes.off
 		self.yawEnabled == False
+
+		self.saveDepth = True
+		self.saveOrientation = True
+
+		self.targetDepth = None
+		self.targetOrientation = None
+
 		self.angleModeSubscriber = rospy.Subscriber("/thrusters/angleMode", StabilityMode, self.depthModeCallback, queue_size=1)
 		self.depthModeSubscriber = rospy.Subscriber("/thrusters/depthMode", StabilityMode, self.angleModeCallback, queue_size=1)
 		self.odomSubcriber = rospy.Subscriber("/odometry/local",Odometry, self.callback, queue_size=1)
+
+		self.stabilityPublisher = rospy.Publisher("/stabiltyWrench", Wrench, queue_size=1)
+
 		self.stabilityWrench = Wrench()
-		self.yawEnabled == False
-		listener = tf.TransformListener()
 
 	def depthModeCallback(self, msg):
 		self.curDepthMode = msg.depthMode
+		if self.curDepthMode != modes.position.value:
+			self.saveDepth = True
 
-	def angleModeCallback(self,msg):
-		if msg.yawEnabled is not self.yawEnabled:
-			self.yawEnabled = msg.yawEnabled
-		
-		else:
-			self.curAngleMode = msg.angleMode
+	def angleModeCallback(self, msg):
+		self.yawEnabled = msg.yawEnabled
+		self.curAngleMode = msg.Mode
+		if self.curAngleMode != modes.position.value:
+			self.saveOrientation = True
 
 	def callback(self, msg):
 		self.pose = msg.pose
 		self.twist = msg.twist
 
-		if self.curDepthMode == depthModes.off.value:
-			self.stabilityWrench.force.x = newStability
-			self.stabilityWrench.force.y = newStability
-			self.stabilityWrench.force.z = newStability
+		if self.curDepthMode == modes.off.value:
+			self.stabilityWrench.force.x = 0
+			self.stabilityWrench.force.y = 0
+			self.stabilityWrench.force.z = 0
 		
-		elif self.curDepthMode == depthModes.velocity.value:
-			unTransformed = - msg.twist.linear.z
-			newStability = listener.transformVector3(base_link, unTransformed)
-			self.stabilityWrench.force.x = newStability
-			self.stabilityWrench.force.y = newStability
-			self.stabilityWrench.force.z = newStability
+		elif self.curDepthMode == modes.velocity.value:
+			quat = rosToArray(msg.pose.orientation)
+			counterVec = rotateVector(quat, [0,0,-10*msg.twist.linear.z])
+			self.stabilityWrench.force.x = counterVec[0]
+			self.stabilityWrench.force.y = counterVec[1]
+			self.stabilityWrench.force.z = counterVec[2]
+
+		elif self.curDepthMode == modes.position.value:
+			if self.saveDepth:
+				self.targetDepth = msg.pose.position.z
+			error = msg.pose.position.z - self.targetDepth
+			quat = rosToArray(msg.pose.orientation)
+			counterVec = rotateVector(quat, [0,0,-10*error])
+			self.stabilityWrench.force.x = counterVec[0]
+			self.stabilityWrench.force.y = counterVec[1]
+			self.stabilityWrench.force.z = counterVec[2]
 
 		else:
-			orientQuaion = listener.transformDATATYPE(base_link, msg.pose)
+			self.stabilityWrench.force.x = 0
+			self.stabilityWrench.force.y = 0
+			self.stabilityWrench.force.z = 0
 
-
-		if self.curAngleMode == angleModes.off.value:
-			self.stabilityWrench.force.x = newStability
-			self.stabilityWrench.force.y = newStability
-			self.stabilityWrench.force.z = newStability	
+		if self.curAngleMode == modes.off.value:
+			self.stabilityWrench.torque.x = 0
+			self.stabilityWrench.torque.y = 0
+			self.stabilityWrench.torque.z = 0
 		
-		elif self.curAngleMode == angleModes.velocity.value:
-			self.stabilityWrench.torque.x = -msg.twist.angular * 10
-			self.stabilityWrench.torque.y = -msg.twist.angular * 10
+		elif self.curAngleMode == modes.velocity.value:
+			self.stabilityWrench.torque.x = -msg.twist.angular.x * 10
+			self.stabilityWrench.torque.y = -msg.twist.angular.y * 10
 
-			if not self.yawEnabled:
-				self.stabilityWrench.torque.z = -msg.twist.angular 			
+			if self.yawEnabled:
+				self.stabilityWrench.torque.z = -msg.twist.angular.z 			
 
-		elif self.curAngleMode == angleModes.position.value:
-			pass
+		elif self.curAngleMode == modes.position.value:
+			if self.saveOrientation:
+				self.targetOrientation = rosToArray(msg.pose.orientation)
+
+			error = tf.transformations.quaternion_multiply(
+				self.targetOrientation,
+				tf.transformations.quaternion_conjugate(rosToArray(msg.pose.orientation))
+			) #Find rotation from current to target position
+
+			#TODO: Do we need to transform the error and how
+			#subLocalError = tf.transformations.quaternion_multiply(
+			#	error, rosToArray(msg.pose.orientation)
+			#) #Rotate rotation from current to target by rotation from world to sub
+
+			rpy = tf.transformations.euler_from_quaternion(error)
+			self.stabilityWrench.torque.x = rpy[0]
+			self.stabilityWrench.torque.y = rpy[1]
+			self.stabilityWrench.torque.z = rpy[2]
 
 		else:
-			pass		
+			self.stabilityWrench.torque.x = 0
+			self.stabilityWrench.torque.y = 0
+			self.stabilityWrench.torque.z = 0
+
+		self.stabilityPublisher.publish(self.stabilityWrench)
+
+if __name__== "__main__":
+	actStab = ActiveStabilizer()
+	rospy.init_node('ActiveStability', anonymous=False)
+	rospy.spin()
