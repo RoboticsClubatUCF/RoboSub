f32e4e9984c4d8f5307beb9142a01aa9c07d390a
Azimath <npnickster@gmail.com>
2018-07-05 16:18:11 -0400

    Added dynamic reconfigure to active stability gains

--- ucf_sub_catkin_ros/src/sub_trajectory/src/active_stability.py
+++ ucf_sub_catkin_ros/src/sub_trajectory/src/active_stability.py
@@ -1,16 +1,15 @@
 #! /usr/bin/env python
 from enum import Enum
+
 from nav_msgs.msg import Odometry
 from sub_trajectory.msg import StabilityMode
 from geometry_msgs.msg import Wrench
+from dynamic_reconfigure.server import Server
+from sub_trajectory.cfg import StabilityConfig
+
 import tf
 import rospy
 import numpy as np
-
-class modes(Enum):
-	off = 1
-	velocity = 2
-	position = 3
 
 def rosToArray(msg): #Convert a ros message with 1-4 dimensions into a numpy array
     return np.array([getattr(msg, key) for key in ("x", "y", "z", "w") if hasattr(msg, key)])
@@ -25,8 +24,8 @@ def rotateVector(quat, vec):
 
 class ActiveStabilizer():
 	def __init__(self):
-		self.curDepthMode = modes.off
-		self.curAngleMode = modes.off
+		self.curDepthMode = StabilityMode.off
+		self.curAngleMode = StabilityMode.off
 		self.yawEnabled = False
 
 		self.saveDepth = True
@@ -34,6 +33,14 @@ class ActiveStabilizer():
 
 		self.targetDepth = None
 		self.targetOrientation = None
+
+		self.depthVelGain = 1
+		self.depthPosGain = 1
+
+		self.orientVelGain = 1
+		self.orientPosGain = 0.1
+
+		self.reconfigureServer = Server(StabilityConfig, self.reconfigureCallback)
 
 		self.angleModeSubscriber = rospy.Subscriber("/thrusters/angleMode", StabilityMode, self.angleModeCallback, queue_size=1)
 		self.depthModeSubscriber = rospy.Subscriber("/thrusters/depthMode", StabilityMode, self.depthModeCallback, queue_size=1)
@@ -43,38 +50,47 @@ class ActiveStabilizer():
 
 		self.stabilityWrench = Wrench()
 
+	def reconfigureCallback(self, config, level):
+		self.depthVelGain = config["depth_vel_P"]
+		self.depthPosGain = config["depth_pos_P"]
+		
+		self.orientVelGain = config["orientation_vel_P"]
+		self.orientPosGain = config["orientation_pos_P"]
+
+		return config
+
 	def depthModeCallback(self, msg):
 		self.curDepthMode = msg.mode
-		if self.curDepthMode != modes.position.value:
+		if self.curDepthMode != StabilityMode.position:
 			self.saveDepth = True
 
 	def angleModeCallback(self, msg):
 		self.yawEnabled = msg.yawEnabled
 		self.curAngleMode = msg.mode
-		if self.curAngleMode != modes.position.value:
+		if self.curAngleMode != StabilityMode.position:
 			self.saveOrientation = True
 
 	def callback(self, msg):
-		if self.curDepthMode == modes.off.value:
+		if self.curDepthMode == StabilityMode.off:
 			self.stabilityWrench.force.x = 0
 			self.stabilityWrench.force.y = 0
 			self.stabilityWrench.force.z = 0
 		
-		elif self.curDepthMode == modes.velocity.value:
+		elif self.curDepthMode == StabilityMode.velocity:
 			quat = rosToArray(msg.pose.pose.orientation)
-			counterVec = rotateVector(quat, [0,0,-1*msg.twist.twist.linear.z])
+			counterVec = rotateVector(quat, [0,0,-self.depthVelGain*msg.twist.twist.linear.z])
 			self.stabilityWrench.force.x = counterVec[0]
 			self.stabilityWrench.force.y = counterVec[1]
 			self.stabilityWrench.force.z = counterVec[2]
 
-		elif self.curDepthMode == modes.position.value:
+		elif self.curDepthMode == StabilityMode.position:
 			if self.saveDepth:
 				self.targetDepth = msg.pose.pose.position.z
 				self.saveDepth = False
 			error = msg.pose.pose.position.z - self.targetDepth
 			quat = rosToArray(msg.pose.pose.orientation)
 			quat = tf.transformations.quaternion_conjugate(quat)
-			counterVec = rotateVector(quat, [0,0,-1*error])
+			counterVec = rotateVector(quat, [0,0,-self.depthPosGain*error])
 			print(counterVec)
 			self.stabilityWrench.force.x = counterVec[0]
 			self.stabilityWrench.force.y = counterVec[1]
@@ -85,21 +101,21 @@ class ActiveStabilizer():
 			self.stabilityWrench.force.y = 0
 			self.stabilityWrench.force.z = 0
 
-		if self.curAngleMode == modes.off.value:
+		if self.curAngleMode == StabilityMode.off:
 			self.stabilityWrench.torque.x = 0
 			self.stabilityWrench.torque.y = 0
 			self.stabilityWrench.torque.z = 0
 		
-		elif self.curAngleMode == modes.velocity.value:
-			self.stabilityWrench.torque.x = -msg.twist.twist.angular.x
-			self.stabilityWrench.torque.y = -msg.twist.twist.angular.y
+		elif self.curAngleMode == StabilityMode.velocity:
+			self.stabilityWrench.torque.x = -self.orientVelGain * msg.twist.twist.angular.x
+			self.stabilityWrench.torque.y = -self.orientVelGain * msg.twist.twist.angular.y
 
 			if self.yawEnabled:
-				self.stabilityWrench.torque.z = -msg.twist.twist.angular.z
+				self.stabilityWrench.torque.z = -self.orientVelGain * msg.twist.twist.angular.z
 			else:
 				self.stabilityWrench.torque.z = 0.0
 
-		elif self.curAngleMode == modes.position.value:
+		elif self.curAngleMode == StabilityMode.position:
 			if self.saveOrientation:
 				self.targetOrientation = rosToArray(msg.pose.pose.orientation)
 				self.saveOrientation = False
@@ -115,10 +131,10 @@ class ActiveStabilizer():
 			#) #Rotate rotation from current to target by rotation from world to sub
 
 			rpy = tf.transformations.euler_from_quaternion(error)
-			self.stabilityWrench.torque.x = rpy[0] * 0.01
-			self.stabilityWrench.torque.y = rpy[1] * 0.1
+			self.stabilityWrench.torque.x = rpy[0] * 0.1 * self.orientPosGain
+			self.stabilityWrench.torque.y = rpy[1] * self.orientPosGain
 			if self.yawEnabled:
-				self.stabilityWrench.torque.z = rpy[2] * 0.1
+				self.stabilityWrench.torque.z = rpy[2] * self.orientPosGain
 			else:
 				self.stabilityWrench.torque.z = 0.0
 
