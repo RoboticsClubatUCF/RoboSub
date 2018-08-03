01fe176f25f56bb877047acd089f42518f0a3a96
Azimath <npnickster@gmail.com>
2018-08-03 16:06:16 -0400

    Add vision manager control and gate feedback usage to state machine

--- ucf_sub_catkin_ros/src/sub_states/src/gate.py
+++ ucf_sub_catkin_ros/src/sub_states/src/gate.py
@@ -4,59 +4,82 @@ import rospy
 import time
 import actionlib
 from actionlib_msgs.msg import GoalStatus
-from sub_vision.msg import TrackObjectAction, TrackObjectGoal, TrackObjectFeedback, TrackObjectResult, VisualServoAction, VisualServoGoal, VisualServoFeedback, VisualServoResult
+from sub_vision.msg import TrackObjectAction, TrackObjectGoal, TrackObjectFeedback, TrackObjectResult
+from sub_vision.msg import VisualServoAction, VisualServoGoal, VisualServoFeedback, VisualServoResult
+from sub_vision.msg import feedback
 from geometry_msgs.msg import  WrenchStamped
 
 from sub_trajectory.msg import StabilityMode
 
 class through(smach.State):
-	def __init__(self):
-		smach.State.__init__(self, outcomes=['preempted', 'success', 'failure'])
-		self.stabilityPub = rospy.Publisher("/thrusters/depthMode", StabilityMode, queue_size=1)
-		self.orientationPub = rospy.Publisher("/thrusters/angleMode", StabilityMode, queue_size=1)
-		self.thruster_pub = rospy.Publisher('/autonomyWrench', WrenchStamped, queue_size=1)
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['preempted', 'success', 'failure'])
+        self.stabilityPub = rospy.Publisher("/thrusters/depthMode", StabilityMode, queue_size=1)
+        self.orientationPub = rospy.Publisher("/thrusters/angleMode", StabilityMode, queue_size=1)
+        self.thruster_pub = rospy.Publisher('/autonomyWrench', WrenchStamped, queue_size=1)
+        self.vision_client = actionlib.SimpleActionClient('track_object', TrackObjectAction)
 
-	def execute(self, userdata):
+        self.autonomyMsg = WrenchStamped()
 
-		self.stabilityMsg = StabilityMode()
-		self.orientationMsg = StabilityMode()
+        self.vision_feedback = rospy.Subscriber('/gate_feedback', feedback, self.feedbackCb)
 
-		self.stabilityMsg.target.z=2
-		self.stabilityMsg.mode = StabilityMode.position
-		self.stabilityPub.publish(self.stabilityMsg)
+    def feedbackCb(self, msg):
+        if msg.center[0]-msg.size[0]/2 < 0:
+            self.autonomyMsg.wrench.force.y = -1 * self.coeff
+            rospy.loginfo_throttle(5, self.autonomyMsg.wrench.force.y)
 
-		self.orientationMsg.target.w=float("nan")
-		self.orientationMsg.mode = StabilityMode.position
-		self.orientationMsg.yawEnabled = True
-		self.orientationPub.publish(self.orientationMsg)
-		
-		self.autonomyMsg = WrenchStamped()
+        elif msg.center[0]-msg.size[0]/2 > 0:
+            self.autonomyMsg.wrench.force.y = 1 * self.coeff
+            rospy.loginfo_throttle(5, self.autonomyMsg.wrench.force.y)
 
-		self.autonomyMsg.wrench.force.x = 2
-		self.thruster_pub.publish(self.autonomyMsg)
+        else:
+            self.autonomyMsg.wrench.force.y = 0
+            rospy.loginfo_throttle(5, self.autonomyMsg.wrench.force.y)
 
-		t0 = time.time()
+    def execute(self, userdata):
 
-		while ((time.time()-t0) < 60) and not self.preempt_requested():
-			time.sleep(0.01)
-			self.autonomyMsg.header.stamp = rospy.Time.now()
-			self.thruster_pub.publish(self.autonomyMsg)
+        self.stabilityMsg = StabilityMode()
+        self.orientationMsg = StabilityMode()
 
-		self.autonomyMsg.wrench.force.x = 0.0
-		self.thruster_pub.publish(self.autonomyMsg)
+        self.stabilityMsg.target.z=2
+        self.stabilityMsg.mode = StabilityMode.position
+        self.stabilityPub.publish(self.stabilityMsg)
 
-		if self.preempt_requested():
-			self.service_preempt()
-			return 'preempted'
+        self.orientationMsg.target.w=float("nan")
+        self.orientationMsg.mode = StabilityMode.position
+        self.orientationMsg.yawEnabled = True
+        self.orientationPub.publish(self.orientationMsg)
 
-		return 'success'
+        goal = TrackObjectGoal()
+        goal.objectType = goal.startGate
+        goal.servoing = True
+        self.vision_client.send_goal(goal)
+
+        self.autonomyMsg.wrench.force.x = 2
+        self.thruster_pub.publish(self.autonomyMsg)
+
+        t0 = time.time()
+
+        while ((time.time()-t0) < 60) and not self.preempt_requested():
+            time.sleep(0.01)
+            self.autonomyMsg.header.stamp = rospy.Time.now()
+            self.thruster_pub.publish(self.autonomyMsg)
+
+        self.autonomyMsg.wrench.force.x = 0.0
+        self.thruster_pub.publish(self.autonomyMsg)
+
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+
+        return 'success'
 
 def makeTask():
-	task = smach.StateMachine(outcomes=['DONE'])
-	with task:
-		smach.StateMachine.add('THROUGH', through(),
-						transitions={'preempted':'DONE',
-							'success': 'DONE',
-							'failure':'THROUGH'})
-	
-	return task
+    task = smach.StateMachine(outcomes=['DONE'])
+    with task:
+        smach.StateMachine.add('THROUGH', through(),
+                        transitions={'preempted':'DONE',
+                            'success': 'DONE',
+                            'failure':'THROUGH'})
+    
+    return task
